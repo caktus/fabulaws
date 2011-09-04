95d0061887034a1cbc77d468eb735927670131b6
Colin Copeland <copelco@caktusgroup.com>
2011-09-04 15:28:55 -0400

    extend EC2Instance with tag support; filter public dns by running instances

--- fabulaws/ec2.py
+++ fabulaws/ec2.py
@@ -49,8 +49,11 @@ class EC2Service(object):
 
     def public_dns(self, filters=None):
         """
-        List all public DNS entries for matching instances
+        List all public DNS entries for all running instances
         """
+        if not filters:
+            filters = {}
+        filters['instance-state-name'] = 'running'
         return [i.public_dns_name for i in self.instances(filters)]
 
 
@@ -67,7 +70,7 @@ class EC2Instance(object):
     _saved_contexts = []
 
     def __init__(self, access_key_id=None, secret_access_key=None,
-                 terminate=True, placement=None):
+                 terminate=True, placement=None, tags=None):
         if not self.ami or not self.user or not self.instance_type:
             raise Exception('You must extend this class and define the ami, '
                             'user, and instance_type class variables.')
@@ -77,6 +80,7 @@ class EC2Instance(object):
         self._secret = secret_access_key or os.environ['AWS_SECRET_ACCESS_KEY']
         self._terminate = terminate
         self._placement = placement
+        self._tags = tags
         self.setup()
 
     def _connect_ec2(self):
@@ -195,6 +199,7 @@ class EC2Instance(object):
         self.elb_conn = self._connect_elb()
         self.key, self.key_file = self._create_key_pair()
         self.instance = self._create_instance()
+        self.add_tags(self._tags)
 
     def add_to_elb(self, elb_name):
         """
@@ -222,6 +227,13 @@ class EC2Instance(object):
             logger.debug('Deleting key file {0}'.format(self.key_file.name))
             self.key_file.close()
             self.key_file = None
+
+    def add_tags(self, tags):
+        """
+        Associate specified tags with instance
+        """
+        if tags:
+            self.conn.create_tags([self.instance.id], tags)
 
     @property
     def hostname(self):
