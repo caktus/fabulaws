4b8cfc230e4605af475641efc945a37a70ff0daa
Tobias McNulty <tobias@caktusgroup.com>
2011-12-20 09:30:02 -0500

    add support for tuning postgres config after installation

--- fabulaws/ubuntu/packages/postgres.py
+++ fabulaws/ubuntu/packages/postgres.py
@@ -1,3 +1,5 @@
+import re
+
 from fabric.api import *
 from fabric.contrib import files
 
@@ -9,16 +11,39 @@ class PostgresMixin(object):
     FabulAWS Ubuntu mixin that installs and configures PostgresSQL.
     """
     postgresql_ppa = None
-    postgresql_packages = ['postgresql', 'postgresql-common']
+    postgresql_packages = ['postgresql', 'libpq-dev']
+    postgresql_tune = False
+    postgresql_tune_type = 'Web'
+    postgresql_shmmax = 536870912 # 512 MB
+
+    @uses_fabric
+    def postgresql_tune_config(self):
+        """Tune the postgresql configuration using pgtune"""
+
+        self.install_packages(['pgtune'])
+        version = run('pg_config --version')
+        version = re.findall(r'(\d+\.\d+)\.?\d+?', version)[0]
+        current = '/etc/postgresql/%s/main/postgresql.conf' % version
+        old = '%s.bak' % current
+        new = '%s.new' % current
+        db_type = self.postgresql_tune_type
+        sudo('pgtune -T %s -i %s -o %s' % (db_type, current, new))
+        sudo('mv %s %s' % (current, old))
+        sudo('mv %s %s' % (new, current))
+        shmmax = self.postgresql_shmmax
+        sudo('sysctl -w kernel.shmmax=%s' % shmmax)
+        files.append('/etc/sysctl.conf', 'kernel.shmmax=%s' % shmmax, use_sudo=True)
+        sudo('service postgresql restart')
 
     def setup(self):
-        """
-        Postgres mixin
-        """
+        """Postgres mixin"""
+
         super(PostgresMixin, self).setup()
         if self.postgresql_ppa:
             self.add_ppa(self.postgresql_ppa)
         self.install_packages(self.postgresql_packages)
+        if self.postgresql_tune:
+            self.postgresql_tune_config()
 
     @uses_fabric
     def create_db_user(self, username, password=None, flags=None):
