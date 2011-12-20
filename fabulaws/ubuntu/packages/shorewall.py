5ee3a2bc2adbe3f15ce9b951bebacd576d026280
Tobias McNulty <tobias@caktusgroup.com>
2011-12-20 09:13:13 -0500

    add support for custom shorewall rules

--- fabulaws/ubuntu/packages/shorewall.py
+++ fabulaws/ubuntu/packages/shorewall.py
@@ -8,6 +8,7 @@ class ShorewallMixin(object):
     FabulAWS Ubuntu mixin that installs and configures the Shorewall firewall
     """
     shorewall_open_ports = ['SSH']
+    shorewall_custom = []
 
     def setup(self):
         """
@@ -23,12 +24,13 @@ class ShorewallMixin(object):
         Configures and starts up a Shorewall firewall on the remote server.
         """
         sudo('apt-get install -y shorewall')
-        rules = '\n'.join(['{0}/ACCEPT\tnet\t\t$FW'.format(p)
-                           for p in self.shorewall_open_ports])
+        rules = ['{0}/ACCEPT net $FW'.format(p)
+                 for p in self.shorewall_open_ports]
+        rules.extend(self.shorewall_custom)
         with cd('/etc/shorewall'):
             sudo('rsync -a /usr/share/doc/shorewall/examples/one-interface/ .')
             if files.exists('shorewall.conf.gz'):
                 sudo('gunzip -f shorewall.conf.gz')
-            files.append('rules', rules, use_sudo=True)
+            files.append('rules', '\n'.join(rules), use_sudo=True)
             sudo('sed -i "s/STARTUP_ENABLED=No/STARTUP_ENABLED=Yes/" shorewall.conf')
         sudo('shorewall start')
