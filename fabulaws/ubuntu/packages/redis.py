d2b51c7071363012c3255f35435d90182d0e63f9
Tobias McNulty <tobias@caktusgroup.com>
2012-01-16 20:22:07 -0500

    support setting redis loglevel & fix restart command

--- fabulaws/ubuntu/packages/redis.py
+++ fabulaws/ubuntu/packages/redis.py
@@ -1,6 +1,3 @@
-import re
-import datetime
-
 from fabric.api import *
 from fabric.contrib import files
 
@@ -14,19 +11,28 @@ class RedisMixin(object):
     """
     redis_ppa = None
     redis_packages = ['redis-server']
-    redis_bind = '127.0.0.1'
+    redis_bind = '127.0.0.1' # set to '' to bind to all interfaces
+    redis_loglevel = 'notice'
     redis_conf = '/etc/redis/redis.conf'
-    redis_bind_pattern = '#?\w*bind 127\.0\.0\.1'
+    redis_bind_pattern = '#?\s*bind 127\.0\.0\.1'
+    redis_loglevel_pattern = '#?\s*loglevel \w+'
 
     @uses_fabric
-    def redis_set_bind_addr(self, bind):
+    def _redis_configure(self, bind=None, loglevel=None):
         if bind is None:
+            bind = self.redis_bind
+        if loglevel is None:
+            loglevel = self.redis_loglevel
+        if bind == '': # all interfaces
             files.comment(self.redis_conf, self.redis_bind_pattern,
                           use_sudo=True)
         else:
-            files.sed(self.redis_conf, self.redis_bind_pattern, self.redis_bind,
+            files.sed(self.redis_conf, self.redis_bind_pattern, bind,
                       use_sudo=True)
-        sudo('service redis-server restart')
+        loglevel = 'loglevel {0}'.format(loglevel)
+        files.sed(self.redis_conf, self.redis_loglevel_pattern, loglevel,
+                  use_sudo=True)
+        sudo('service redis-server restart', pty=False) # must pass pty=False
 
     def setup(self):
         """Redis mixin"""
@@ -35,4 +41,4 @@ class RedisMixin(object):
         if self.redis_ppa:
             self.add_ppa(self.redis_ppa)
         self.install_packages(self.redis_packages)
-        self.redis_set_bind_addr(self.redis_bind)
+        self._redis_configure()
